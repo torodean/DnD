648e6fbff6455c16324208275e98ec8ab518bb6f
Antonius Torode <24546058+torodean@users.noreply.github.com>
2023-06-07 08:59:27 -0500

    Create format_text.py

---
+++ campaign/quests/manastorm_legal_advisors/format_text.py
@@ -0,0 +1,44 @@
+import argparse
+import re
+
+def format_single_line(text):
+    """
+    Formats the input text into a single line with proper spacings.
+
+    Args:
+        text (str): The input text to format.
+
+    Returns:
+        str: The formatted text in a single line.
+    """
+    # Remove newline characters and tabs
+    text = text.replace('\n', '').replace('\t', '')
+
+    # Add spaces after punctuations
+    punctuations = ['.', ',', ';', ':', '!', '?']
+    for punctuation in punctuations:
+        text = text.replace(punctuation, punctuation + ' ')
+
+    # Replace multiple consecutive spaces with a single space
+    text = re.sub(' +', ' ', text)
+
+    # Remove extra spaces before and after the text
+    text = text.strip()
+
+    return text
+
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', help='Specify the input file', required=True)
+args = parser.parse_args()
+
+# Read input file
+file_path = args.file
+with open(file_path, 'r') as file:
+    input_text = file.read()
+
+# Format input text to a single line
+formatted_text = format_single_line(input_text)
+print(formatted_text)
+
