e5b9769e2b852810de82959820fcb242118b9715
Antonius Torode <24546058+torodean@users.noreply.github.com>
2024-02-16 15:13:04 -0600

    Update stockpile-cron.sh

--- templates/stockpile/stockpile-cron.sh
+++ templates/stockpile/stockpile-cron.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-#TODO - This is just a mock script. Paths and things need finalized.
+script_dir=$(pwd)
+stockpile_dir=${script_dir}/../../campaign/notes/stockpile
 
 # Ensure we have the latest changes.
 cd ~/DnD/templates/stockpile
 git pull
 
 # Run the stockpile updates.
-./stockpile.py -g stockpile_master_general.txt -t stockpile_master_trade.txt -o stockpile_inventory_lists.input -b stockpile_buy_history.txt -s stockpile_sell_history.txt
+./stockpile.py -g ${script_dir}/stockpile_master_general.txt -t ${script_dir}/stockpile_master_trade.txt -o ${script_dir}/stockpile_inventory_lists.input -b ${script_dir}/stockpile_buy_history.txt -s ${script_dir}/stockpile_sell_history.txt
 
 # Update the plots.
-./stockpile_plot.py -b stockpile_buy_history.txt -s stockpile_sell_history.txt -o plots
+./stockpile_plot.py -b ${script_dir}/stockpile_buy_history.txt -s ${script_dir}/stockpile_sell_history.txt -o ${stockpile_dir}/plots
 
 # Update the HTML page.
-#TODO - finish adding -f capabilities to creator.py
-../creator.py -f stockpile/
+../creator.py -f ${script_dir}/stockpile_inventory_lists.input
 
 git add -A
 git commit -m "Automatic stockpile update."
 git push origin main
 
 # Update the public MMORPDND database to reflect changes
-# TODO
 cd ~/MMORPDND.github.io/
 git pull
 ./updateSubmodules.sh
