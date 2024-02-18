2743b11bb83a273125a31327075668b5731344e4
Antonius Torode <awtorode@gmail.com>
2024-02-18 11:06:39 -0600

    Automatic stockpile update.

--- templates/stockpile/stockpile-cron.sh
+++ templates/stockpile/stockpile-cron.sh
@@ -4,24 +4,31 @@ script_dir=$(pwd)
 stockpile_dir=${script_dir}/../../campaign/notes/stockpile
 
 # Ensure we have the latest changes.
-cd ~/DnD/templates/stockpile
+echo "Updating git repo and files to latest!"
+cd ~/git/DnD/templates/stockpile
 git pull
 
 # Run the stockpile updates.
+echo "Running the stockpile update!"
 ./stockpile.py -g ${script_dir}/stockpile_master_general.txt -t ${script_dir}/stockpile_master_trade.txt -o ${script_dir}/stockpile_inventory_lists.input -b ${script_dir}/stockpile_buy_history.txt -s ${script_dir}/stockpile_sell_history.txt
 
 # Update the plots.
+echo "Running the stockpile plot generation!"
 ./stockpile_plot.py -b ${script_dir}/stockpile_buy_history.txt -s ${script_dir}/stockpile_sell_history.txt -o ${stockpile_dir}/plots
 
 # Update the HTML page.
+echo "Updating the stockpile HTML pages!"
 ../creator.py -f ${script_dir}/stockpile_inventory_lists.input
 
+# Add the new updated files to the git repo.
+echo "Updating the git repo with the new files!"
 git add -A
 git commit -m "Automatic stockpile update."
 git push origin main
 
-# Update the public MMORPDND database to reflect changes
-cd ~/MMORPDND.github.io/
+# Update the public MMORPDND database to reflect changes.
+echo "Updating the MMORPDND git database with the new files!"
+cd ~git/MMORPDND.github.io/
 git pull
 ./updateSubmodules.sh
 git add -A
